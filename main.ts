116cdaa81465018274889eb599302175d2193201
A4And <bernot.and@gmail.com>
2026-03-08 08:36:32 +0100

    Add functions to read pin values and measurements

--- main.ts
+++ main.ts
@@ -6,3 +6,19 @@ namespace monExtension {
 
 - `block="..."` définit le nom de la catégorie de blocs
 - `color` et `icon` personnalisent l’apparence dans l’éditeur
+
+//% block="lire valeur sur broche %pin"
+export function lire(pin: DigitalPin): number {
+    return pins.digitalReadPin(pin)
+}
+export enum Mesure {
+    //% block="température"
+    Temperature = 0,
+    //% block="humidité"
+    Humidite = 1
+}
+
+//% block="lire %what"
+export function lireMesure(what: Mesure): number {
+    return 0
+}
