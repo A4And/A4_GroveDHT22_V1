fb489ecb52cd55f36f24007492a6c0b8f9aaf7dd
A4And <bernot.and@gmail.com>
2026-03-08 08:33:20 +0100

    Define namespace and add comments in main.ts
    Add namespace definition and comments for extension configuration

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
+```typescript
+//% color=#2F5597 icon="\uf2c9" block="Mon Extension"
+namespace monExtension {
+}
+```
 
+- `block="..."` définit le nom de la catégorie de blocs
+- `color` et `icon` personnalisent l’apparence dans l’éditeur
